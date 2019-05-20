f5aff6e8117f6462a484bbbac98232124cb99e57
madhubabu-nyros <madhubabu.nyros@gmail.com>
2019-05-20 19:13:56 +0530

    USERrGISTRATIONFORM INIT COMMIT

---
+++ assets/client.js
@@ -0,0 +1,92 @@
+const button = document.getElementById('submit');
+button.addEventListener('click', function(e) {
+  const user = document.getElementById('user').value;
+  const password = document.getElementById('password').value;
+  const email = document.getElementById('email').value;
+  const mobile = document.getElementById('mobile').value;
+  var flag = 1;
+  if(user=="" || password == "" || email=="" || mobile=="") {
+     flag = 0;
+     document.getElementById("err_msg").innerHTML = "<div id='user_failure'>Enter Your Details</div>";
+  }
+  
+  var data = {name:user, email:email, mobile:mobile, password:password};
+  if(flag==1) {
+  fetch('/createUser', {method: 'POST',headers: {'Accept': 'application/json, text/plain, */*',
+  'Content-Type': 'application/json'},dataType:'json', body:JSON.stringify(data)})
+      .then(function(response) {
+          return response.json();
+       })   
+      .then(function(myJson){
+        console.log('response:',JSON.stringify(myJson));
+        var res = JSON.stringify(myJson);
+        var error_message = JSON.parse(res);
+        if(error_message.status===200) {
+          document.getElementById("err_msg").innerHTML = "<div id='user_success'>"+error_message.message+"</div>";
+          getUsers();
+          return;
+        }
+        else{
+          document.getElementById("err_msg").innerHTML = "<div id='user_failure'>"+error_message.message+"</div>";
+        }
+        
+      })
+        .catch(function(error) {
+          console.log(error);
+          throw new Error('Request failed.');
+        });
+    }    
+});
+getUsers();
+function getUsers() {
+      fetch('/getUsers', {method: 'POST'})    
+       .then(function(responce){
+           return responce.json();
+       })
+       .then(function(myJson){
+          var res = JSON.stringify(myJson);
+          var result2 = JSON.parse(res);
+          var users='';
+          for(var i=0;i<result2.data.length;i++)
+          { 
+            
+            users+= '<div class="col-md-3 users"><div class="name">'+result2.data[i].name+'</div><div class="email">'+result2.data[i].email+'</div><div class="mobile">'+result2.data[i].mobile+'</div><div id="buttons"><button class="btn btn-info btn-sm"><a  href="/user_details?id='+result2.data[i]._id+'">View</a></button><button class="btn btn-danger btn-sm" onclick=delete_users("'+result2.data[i].mobile+'")>Delete</button></div></div>'; 
+          }
+            document.getElementById("display_users").innerHTML = users;
+
+      }) 
+}
+function delete_users(mobile) {
+  var data = {mobile:mobile};
+  
+      var x = confirm("Are you sure you want to delete?");
+      if (x) {
+      fetch('/deleteUsers', {method: 'POST',headers: {'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json'},dataType:'json', body:JSON.stringify(data)})
+      .then(function(responce) {
+           return responce.json();
+      })
+      .then(function(myJson) {
+        var res = JSON.stringify(myJson);
+        var result3 = JSON.parse(res);
+        if(result3.status===200) {
+          document.getElementById("err_msg").innerHTML= "<div id='user_failure'>"+result3.message+"</div>";
+          getUsers();
+        } else {
+          document.getElementById("err_msg").innerHTML = result3.message;
+        }
+
+      })
+      } else { 
+        return false;
+      } 
+     
+}
+// function update_userdetails(_id){
+//   var data = {_id:_id}
+//   fetch('/updateUsers', {method: 'POST',headers: {'Accept': 'application/json, text/plain, */*',
+//       'Content-Type': 'application/json'},dataType:'json', body:JSON.stringify(data)})
+
+// }
+
+  
---
+++ server.js
@@ -0,0 +1,147 @@
+var express = require("express");
+var bodyParser  = require("body-parser");
+var app =  express();
+var cors = require('cors');
+var ObjectId = require('mongodb').ObjectId;
+const MongoClient = require('mongodb').MongoClient;
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+app.use(express.static('assets'));
+let db;
+var url = "mongodb://localhost:27017/ecommerce";
+
+MongoClient.connect(url,(err, database) =>
+{
+  if(err)
+  {
+  	return console.log(err);
+  }
+  db = database;
+});
+
+app.get('/',function(req,res){
+  res.sendFile(__dirname+"index.html");
+});
+app.use(cors());
+app.post('/createUser',function(req,res){
+    var user_name=req.body.name;
+    var password=req.body.password;
+    var email=req.body.email;
+    var mobile=req.body.mobile;
+    var data = {name:user_name, email:email, mobile:mobile, password:password};
+    db.collection('userdetails').findOne({mobile:mobile}, function(err, result) {
+      if(result==null) { 
+          db.collection('userdetails').insertOne(data, (err, result) => {
+            if (err) {
+              res.json({
+                  status: 401,
+                  message:'Something went wrong',
+                  err: err
+              })
+            } else {
+              res.json({
+                  status: 200,
+                  message:'User created successfully!',
+                  data: result
+              })
+            }
+          });
+      } else {
+            res.json({
+              status: 401,
+              message:'Mobile number alredy exist',
+              err:err
+            })
+      }
+    });
+
+})
+app.post('/getUsers',(req, res) => {
+  db.collection('userdetails').find({}).toArray(function(err, result) {
+      if(err) {
+            res.json({
+              status: 401,
+              message: 'Something went wrong',
+              err: err
+            })
+      } else {
+              res.json({
+                status:200,
+                message:'User Details',
+                data: result
+              })
+      }
+      
+  })
+})
+app.post('/deleteUsers',(req, res) => {
+    console.log("deleteUsers:");
+    console.log("req.body:",req.body)
+    var user_name=req.body.name;
+    var password=req.body.password;
+    var email=req.body.email;
+    var mobile=req.body.mobile;
+    var data = {name:user_name, email:email, mobile:mobile, password:password};
+    db.collection('userdetails').findOne({mobile:mobile}, function(err, result) {
+      console.log("result:",result)
+    if(result!=null) {
+      db.collection('userdetails').deleteOne(result, function(err, result) {
+        if(err) {
+                res.json({
+                  status:401,
+                  message: 'Something went wrong',
+                  err: err
+                })
+        } else {
+                res.json({
+                  status:200,
+                  message:'Delete successfully',
+                  data: result
+                })
+        }
+      })
+    } else {
+                res.json({
+                  status:401,
+                  message: 'The User Name not found',
+                  err: err
+                })
+
+    }
+
+    })
+})
+app.get('/user_details',function(req,res){
+  res.sendFile(__dirname+"/assets/view.html");
+});
+app.post('/getUserDetails', (req, res) => {
+  var id = req.body._id;
+  db.collection('userdetails').findOne({_id:ObjectId(id)}, function(err, result) {
+     console.log('err', err);
+     console.log('result', result);
+      if(err || result==null) {
+            res.json({
+              status: 401,
+              message: 'User Details not found',
+              err: err
+            })
+      } else {
+              res.json({
+                status:200,
+                message:'User Details',
+                data: result
+              })
+      }
+      
+  })
+})
+// app.post('/updateUsers', function(req, res){
+//   var id = req.body._id;
+//   db.collection('userdetails').findOne({_id:ObjectId(id)}, function(err, result) {
+//      db.collection('userdetails').update      
+//   })
+// }) 
+
+app.listen(5000,function(){
+  console.log("Started on PORT 5000");
+})
